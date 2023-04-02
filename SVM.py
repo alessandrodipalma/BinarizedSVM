917e0b997fe26881f5669f8b5a69529bf028319a
alessandrodipalma <a.dipalma9@studenti.unipi.it>
2023-04-02 17:21:20 +0200

    fix F initialization bug

--- SVM.py
+++ SVM.py
@@ -1,65 +1,101 @@
 from column_generation import column_generation
+import numpy as np
+from matplotlib import pyplot as plt
+from prettytable import PrettyTable
 
 class BinarizedSVM:
 
-    def __init__(C=1):
+    def __init__(self, C=1, verbose=False):
         self.C = C
+        self.verbose = verbose
 
     def fit(self, X, y, predictor_variables_names=None):
-        if len(predictor_variables_name) != X.shape[1]:
+        """
+
+        :param X: bidimensional numpy array of shape (n,m), containing the n training samples
+        :param y: unidimensional numpy array of len n, containing the labels of the n training samples. The labels of the two classes must be [1,-1]
+        :param predictor_variables_names: iterable of m string, containing the names of the predictor variables names of X
+        :return:
+        """
+
+        if predictor_variables_names is None:
+            predictor_variables_names = [str(i) for i in range(X.shape[1])]
+        elif len(predictor_variables_names) != X.shape[1]:
             print(f"Predictor variables names should match input shape {X.shape}")
             return
-        
+
+        if X.shape[0] != y.shape[0]:
+            print("Esempi ed etichette devono avere la stessa lunghezza.")
+            return
+
+        if self.verbose:
+            print(f"Nomi delle variabili: {predictor_variables_names}")
+
         self.predictor_variables_names = predictor_variables_names
         self.n_predictor_vars = len(self.predictor_variables_names)
-        
-        self.lambda_star, self.F, self.soglie = column_generation(X, y, self.C)
-        print("Fitting completed")
+        self.lambda_star, self.F, self.soglie, stats = column_generation(X, y, self.C, verbose=self.verbose)
 
-        omega_star = {}
+        if self.verbose:
+            print(f"\nAddestramento completato in {stats['iter_count']} iterate")
 
-        for l in soglie.keys():
-            omega_star[l] = np.zeros(len(soglie[l]))
-            for b, phi in enumerate(soglie[l]):
+        self.omega_star = {}
+
+        for l in self.soglie.keys():
+            self.omega_star[l] = np.zeros(len(self.soglie[l]))
+            for b, phi in enumerate(self.soglie[l]):
                 for i in range(len(X)):
-                    c = 1 if X[i,l] > phi else -1
-                    omega_star[l][b] += lambda_star[i] * c
+                    c = 1 if X[i, l] > phi else -1
+                    self.omega_star[l][b] += self.lambda_star[i] * c
 
-        self.omega_star = omega_star        
+
+        if self.verbose:
+            table = PrettyTable()
+            table.field_names = ["Predictor var", "soglia","peso"]
+            print("Le soglie selezionate sono:")
+            for l in self.soglie.keys():
+                for b, phi in enumerate(self.soglie[l]):
+                    if b==0:
+                        table.add_row([f"{l}, {len(self.soglie[l]) } features", self.soglie[l][b], self.omega_star[l][b]])
+                    else:
+                        table.add_row(["", self.soglie[l][b], self.omega_star[l][b]])
+
+            print(table)
 
     def visualizza_soglie(self, out_dir=None):
+
         for predictor_variable, thresholds in self.soglie.items():
-            sorted_soglie = sorted(list(zip(soglie[predictor_variable], omega_star[predictor_variable])), reverse=False)
+
+            sorted_soglie = sorted(list(zip(self.soglie[predictor_variable], self.omega_star[predictor_variable])),
+                                   reverse=False)
             soglies = [v for (v, l) in sorted_soglie]
             omegas = [l for (v, l) in sorted_soglie]
 
-            print(soglies, omegas)
+
             plt.step(soglies, omegas)
-            plt.xlabel('Threshold')
+            plt.xlabel('soglia')
             plt.ylabel('Weight')
-            plt.title(f"Pesi per le soglie della feature {predictor_variable}")
+            plt.title(f"Pesi per var {predictor_variable}")
             if out_dir is None:
                 plt.show()
             else:
                 plt.savefig(f"{out_dir}{predictor_variable}")
 
-    
     def predict(self, X):
-        return np.array(list(map(self.score_function, x)))trn 
+        return np.array(list(map(self.score_function, X)))
 
-    
     def score_function(self, x):
         sum_ = 0
         n_predictor_vars = len(self.soglie)
         for l in range(n_predictor_vars):
-            for j, b in enumerate(soglie[l]):
-                if x[l] >= b:
-                    sum_ += omega_star[l][j]
+            for j, phi in enumerate(self.F[l]):
+                sum_ += self.omega_star[l][j] * phi(x[l])
 
-        score = sum_ #+ B
+        score = sum_  # + B
         # print(score)
         if score > 0:
             return 1
         else:
             return -1
+
+
 
--- column_generation.py
+++ column_generation.py
@@ -84,22 +84,22 @@ def solve_6F_dual(x, y, C, F):
     return lambda_star
 
 
-def solve_6F_dual_with_cvpy(x, c, C, F):
+def solve_6F_dual_with_cvpy(x, c, C, F, verbose=False):
     I = len(x)
     lambda_ = cvxpy.Variable(I)
     objective = cvxpy.Maximize(cvxpy.sum(lambda_))
     constraints = []
     for l in F.keys():
         for phi in F[l]:
-            coeff = np.array([c[u] * phi[u] for u in range(I)])
+            coeff = np.array([c[u] * phi(x[u,l]) for u in range(I)])
             constraints.extend([coeff.T @ lambda_ >= -1.0, coeff.T @ lambda_ <= 1.0])
 
-    constraints.append(c.T @ lambda_ == 0)
+    constraints.append(c.T @ lambda_ == 0.0)
     constraints.extend([lambda_ >= 0.0, lambda_ <= C])
 
     problem = cvxpy.Problem(objective, constraints)
 
-    problem.solve(solver='ECOS', verbose=True)
+    problem.solve(solver='ECOS', verbose=verbose)
 
     return lambda_.value
 
@@ -115,76 +115,60 @@ def mediane(x):
 
     for l in range(number_of_predictor_variables):
         b_star_l = np.median(x[:, l])
-        phi_star_l = np.zeros(len(x))
+        phi_star_l = lambda x: 1 if x > b_star_l else 0
         soglie[l].append(b_star_l)
-        for i in range(len(x)):
-            if x[i, l] >= b_star_l:
-                phi_star_l[i] = 1
-            else:
-                phi_star_l[i] = -1
         F0[l].append(phi_star_l)
 
     return F0, soglie
 
 
-def gamma(phi, lambda_star, labels):
+def gamma(x, b, l, lambda_star, labels):
+    phi = lambda x: 1 if x >= b else 0
     sum_ = 0
 
-    for u in range(len(phi)):
-        sum_ += lambda_star[u] * labels[u] * phi[u]
+    for u in range(x.shape[1]):
+        sum_ += lambda_star[u] * labels[u] * phi(x[l,u])
 
-    return sum_
+    return sum_, phi
 
 
-def column_generation(x, labels, C):
+def column_generation(x, labels, C, max_iter=1000, verbose=False):
     number_of_predictor_variables = len(x[0])
 
     # inizializzazione di F
+    if verbose: print("initializing F...")
     F, soglie = mediane(x)
 
     iter_count = 0
     F_modified = True
 
-    while F_modified:  # Step 3: se F non è stato modificato, abbiamo trovato la soluzione ottima di (6)
-        print(f"iteration {iter_count}" + '-' * 100)
-        lambda_star = solve_6F_dual_with_cvpy(x, labels, C, F)
+    while F_modified and max_iter>iter_count:  # Step 3: se F non è stato modificato, abbiamo trovato la soluzione ottima di (6)
+        if verbose: print(f"iteration {iter_count}" + '-' * 100)
+        lambda_star = solve_6F_dual_with_cvpy(x, labels, C, F, False)
         # Step 2: calcolo delle φlb^+_l e φlb^-_l e aggiornamento di F se necessario
         F_modified = False
-        for l in range(number_of_predictor_variables):
-            print(f"scelgo le soglie per {l}...")
-            b_plus_l, b_minus_l = scegli_soglie(x[:, l], labels, lambda_star)
-            print(f"soglie trovate: b_plus:{b_plus_l}\t b_minus:{b_minus_l}")
 
-            phi_plus_l = [1 if x[i, l] >= b_plus_l else 0 for i in range(len(x))]
-            gamma_plus_l = gamma(phi_plus_l, lambda_star, labels)
+        for l in range(number_of_predictor_variables):
+            if verbose: print(f"scelgo le soglie per {l}...")
+
+            b_plus_l, b_minus_l = scegli_soglie(x[:, l], labels, lambda_star)
+
+            if verbose: print(f"soglie trovate: b_plus:{b_plus_l}\t b_minus:{b_minus_l}")
+
+            gamma_plus_l, phi_plus_l = gamma(x, b_plus_l, l, lambda_star, labels)
             if gamma_plus_l > 1:
-                F[l].append(np.array(phi_plus_l))
+                F[l].append(phi_plus_l)
                 soglie[l].append(b_plus_l)
                 F_modified = True
 
-            phi_minus_l = [1 if x[i, l] >= b_minus_l else 0 for i in range(len(x))]
-            gamma_minus_l = gamma(phi_minus_l, lambda_star, labels)
+            gamma_minus_l, phi_minus_l = gamma(x, b_minus_l, l, lambda_star, labels)
             if gamma_minus_l < -1:
-                F[l].append(np.array(phi_minus_l))
+                F[l].append(phi_minus_l)
                 soglie[l].append(b_minus_l)
                 F_modified = True
 
         iter_count += 1
 
-    return lambda_star, F, soglie
+    stats = {"iter_count": iter_count}
 
-
-def score_function(x, omega, soglie, B):
-    sum_ = 0
-
-    for predictor_var in range(len(x)):
-        for b in soglie[predictor_var]:
-            if x[predictor_var] > b:
-                sum_ += omega[predictor_var]
-
-    score = sum_ + B
-
-    if score > 0:
-        return 1
-    else:
-        return -1
+    return lambda_star, F, soglie, stats
